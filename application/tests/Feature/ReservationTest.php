e619b0c827784e098919ecff4193e1295ffde9ae
Andrey Sergeev <andreylong@gmail.com>
2020-02-04 20:32:33 +0300

    backend test - finished

--- application/tests/Feature/ReservationTest.php
+++ application/tests/Feature/ReservationTest.php
@@ -2,17 +2,15 @@
 
 namespace Tests\Feature;
 
-use App\Http\Middleware\VerifyCsrfToken;
 use App\Model\EventLocation;
 use App\Model\Place;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class ReservationTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseMigrations;
 
     public function testReservationValidationFailure()
     {
@@ -47,12 +45,14 @@ class ReservationTest extends TestCase
         factory(EventLocation::class)->create();
         factory(Place::class)->create();
 
+        $logoImage = UploadedFile::fake()->create('logo_test.jpg', 1024 * 2);
+
         $response = $this->json('POST','/api/reserve_place', [
             'company_name' => 'Test',
             'contact_name' => 'Test',
             'email' => 'and',
             'phone' => '375259759933',
-            'logo_file' => '',
+            'logo_file' => $logoImage,
             'place_id' => 1001
         ]);
 
@@ -60,8 +60,113 @@ class ReservationTest extends TestCase
                  ->assertExactJson([
                      'message'   => 'The given data was invalid.',
                      'errors'    => [
-                         'email' => ['The email field is required.']
+                         'email' => ['The email must be a valid email address.']
                      ]
                  ]);
+    }
+
+    public function testReservationValidationFailurePhone()
+    {
+        factory(EventLocation::class)->create();
+        factory(Place::class)->create();
+
+        $logoImage = UploadedFile::fake()->create('logo_test.jpg', 1024 * 2);
+
+        $response = $this->json('POST','/api/reserve_place', [
+            'company_name' => 'Test',
+            'contact_name' => 'Test',
+            'email' => 'test@test.com',
+            'phone' => '375',
+            'logo_file' => $logoImage,
+            'place_id' => 1001
+        ]);
+
+        $response->assertStatus(422)
+                 ->assertExactJson([
+                     'message'   => 'The given data was invalid.',
+                     'errors'    => [
+                         'phone' => ['The phone must be a valid phone number']
+                     ]
+                 ]);
+    }
+
+    public function testReservationValidationFailureLogo()
+    {
+        factory(EventLocation::class)->create();
+        factory(Place::class)->create();
+
+        $response = $this->json('POST','/api/reserve_place', [
+            'company_name' => 'Test',
+            'contact_name' => 'Test',
+            'email' => 'test@test.com',
+            'phone' => '375259759933',
+            'logo_file' => 'qwe',
+            'place_id' => 1001
+        ]);
+
+        $response->assertStatus(422)
+                 ->assertExactJson([
+                     'message'   => 'The given data was invalid.',
+                     'errors'    => [
+                         'logo_file' => [
+                             'The logo file must be a file.',
+                             'The logo file must be a file of type: jpeg, png.'
+                         ]
+                     ]
+                 ]);
+    }
+
+    public function testReservationValidationFailurePlace()
+    {
+        factory(EventLocation::class)->create();
+        factory(Place::class)->create();
+
+        $logoImage = UploadedFile::fake()->create('logo_test.jpg', 1024 * 2);
+
+        $response = $this->json('POST','/api/reserve_place', [
+            'company_name' => 'Test',
+            'contact_name' => 'Test',
+            'email' => 'test@test.com',
+            'phone' => '375259759933',
+            'logo_file' => $logoImage,
+            'place_id' => 10
+        ]);
+
+        $response->assertStatus(422)
+                 ->assertExactJson([
+                     'message'   => 'The given data was invalid.',
+                     'errors'    => [
+                         'place_id' => ['The selected place id is invalid.']
+                     ]
+                 ]);
+    }
+
+    public function testAddReservation()
+    {
+        factory(EventLocation::class)->create();
+        factory(Place::class)->create();
+
+        $logoImage = UploadedFile::fake()->create('logo_test.jpg', 1024 * 2);
+
+        $response = $this->json('POST','/api/reserve_place', [
+            'company_name' => 'Test',
+            'contact_name' => 'Test',
+            'email' => 'test@test.com',
+            'phone' => '375259759933',
+            'logo_file' => $logoImage,
+            'place_id' => 1001
+        ]);
+
+        $data = json_decode($response->getContent());
+
+        $response->assertStatus(201)
+                 ->assertExactJson([
+                     'data' => [
+                         'id'   => 1,
+                         'logo' => $data->data->logo,
+                         'place_id' => 1001
+                     ]
+                 ]);
+
     }
 }
