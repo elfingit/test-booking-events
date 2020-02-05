047910e6957402f9bb4d2fdffcd6abaee222e2dc
Andrey Sergeev <andreylong@gmail.com>
2020-02-05 10:44:51 +0300

    frontend tests - started

---
+++ application/resources/js/__tests__/app.test.js
@@ -0,0 +1,22 @@
+import { mount } from "@vue/test-utils";
+import App from "../components/App";
+
+describe('App', () => {
+    test('Is App a vue instance?', () => {
+        const wrapper = mount(App, {
+            stubs: ['router-view']
+        })
+
+        expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+
+    test('Is App render container as div?', () => {
+        const wrapper = mount(App, {
+            stubs: ['router-view']
+        })
+
+        const el = wrapper.find('.container')
+        expect(el.is('div')).toBe(true)
+
+    })
+})
---
+++ application/resources/js/__tests__/loader.test.js
@@ -0,0 +1,18 @@
+import { mount } from "@vue/test-utils";
+import Loader from "../components/Loader";
+
+describe('Loader', () => {
+    test('Is Loader a vue instance?', () => {
+        const wrapper = mount(Loader)
+
+        expect(wrapper.isVueInstance()).toBeTruthy()
+    })
+
+    test('Is Loader render container as div?', () => {
+        const wrapper = mount(Loader)
+
+        const el = wrapper.find('.loader-container')
+        expect(el.is('div')).toBe(true)
+
+    })
+})
